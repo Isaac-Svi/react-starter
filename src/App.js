dc499dd01ac6d0de0384d2305fe146d88371cc1b
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-02-16 22:47:03 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+import SVG from './assets/react-icon.svg'
+
+const App = () => {
+  return (
+    <>
+      <h2>React Starter</h2>
+      <img src={SVG} alt='react logo' />
+    </>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: __dirname + '/src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+    compress: true,
+    historyApiFallback: true,
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: false,
+    },
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/public/index.html',
+    }),
+  ],
+}
